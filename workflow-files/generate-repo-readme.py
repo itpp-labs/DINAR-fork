f9cce3f4ae3d7bad596fe625d8fd1e8b742e30f2
Ivan Kropotkin <yelizariev@itpp.dev>
2022-04-20 17:03:51 +0200

    disable unmaintained repos

--- workflow-files/generate-repo-readme.py
+++ workflow-files/generate-repo-readme.py
@@ -24,11 +24,11 @@ from plumbum.cmd import cat
 BRANCHES = ["15.0", "14.0", "13.0", "12.0", "11.0", "10.0", "9.0", "8.0"]
 REPOS = {
     "pos-addons": {"extra_branches": ["7.0"]},
-    "mail-addons": {},
-    "misc-addons": {"extra_branches": ["7.0"]},
+    #"mail-addons": {},
+    #"misc-addons": {"extra_branches": ["7.0"]},
     "sync-addons": {},
     "access-addons": {},
-    "website-addons": {},
+    #"website-addons": {},
 }
 ODOO_VERSIONS = ["15.0", "14.0", "13.0", "12.0", "11.0", "10.0", "9.0", "8.0", "7.0"]
 
